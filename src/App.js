3ceab8ea5664d067bc7b94e6d7d0e31a237eebc9
Tayamul Rai <Tayamul@Bhumikas-MacBook-Pro.local>
2023-01-09 22:22:32 +0000

    Import CssBaseline in the App file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline';
 import Header from './components/Header'
+import Articles from './components/Articles'
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline/>
       <Header />
+      <Routes>
+        <Route path='/' element={<Articles />}></Route>
+      </Routes>
     </div>
   );
 }
