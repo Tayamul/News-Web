4055d1c7d22b9e59bb79ee8908b2e7dcee436afd
Tayamul Rai <Tayamul@Bhumikas-MacBook-Pro.local>
2023-01-09 17:28:48 +0000

    Add required components

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <select>
+            <option value="categories">Categories</option>
+            <option value="coding">Coding</option>
+            <option value="football">Football</option>
+            <option value="cooking">Cooking</option>
+        </select>
+        
+    </nav>
+  )
+}
+
+export default Nav
