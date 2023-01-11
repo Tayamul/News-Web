bedada16477ac9af3cf2af6401ad878cb8f693c1
Tayamul <rai_tayamul@hotmail.com>
2023-01-11 08:10:10 +0000

    Add new route to a single article page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Articles from './components/Articles'
+import SingleArticle from './components/SingleArticle';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Articles />}></Route>
+        <Route path='/articles/:article_id' element={<SingleArticle />}></Route>
       </Routes>
     </div>
   );
