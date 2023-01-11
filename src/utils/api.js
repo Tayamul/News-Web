691f06e2d219e47502313ff750991f01effb176e
Tayamul <rai_tayamul@hotmail.com>
2023-01-11 08:11:27 +0000

    Add a new fetch request to get signle article by their id

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,3 +9,9 @@ export const getArticles = () => {
         return response.data})
 }
 
+export const getArticlesById = (article_id) => {
+    return newsApi.get(`/articles/${article_id}`).then((response) => {
+        console.log(response);
+        return response.data;
+    })
+}
