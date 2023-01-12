6d7b67aef64ab079137179766a404d2fa8ae64db
Tayamul <rai_tayamul@hotmail.com>
2023-01-12 15:33:36 +0000

    Render Nav component in the app file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
+import {useState} from 'react'
 import Header from './components/Header'
+import Nav from './components/Nav'
 import Articles from './components/Articles'
 import SingleArticle from './components/SingleArticle';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
   return (
     <div className="App">
-      
       <Header />
+      <Nav setArticles={setArticles}/>
       <Routes>
-        <Route path='/' element={<Articles />}></Route>
+        <Route path='/' element={<Articles articles={articles} setArticles={setArticles}/>}></Route>
         <Route path='/articles/:article_id' element={<SingleArticle />}></Route>
       </Routes>
     </div>
