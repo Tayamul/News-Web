831133485260875c09aa3610900d6f592e399716
Tayamul <rai_tayamul@hotmail.com>
2023-01-12 16:55:39 +0000

    Edit required changes to the component

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -1,44 +1,52 @@
-import React, { useState } from 'react'
-import * as api from '../utils/api'
-import './commentAdder.css'
+import React, { useState } from "react";
+import * as api from "../utils/api";
+import "./commentAdder.css";
 
-const CommentAdder = ({article_id, setComments}) => {
-    const [newComment, setNewComment] = useState("")
-    const [isSuccessful, setIsSuccessful] = useState(false)
-    const [error, setError] = useState(null)
+const CommentAdder = ({ article_id, setComments }) => {
+  const [newComment, setNewComment] = useState("");
+  const [isSuccessful, setIsSuccessful] = useState(false);
+  const [error, setError] = useState(null);
 
-    const changeHandler = (e) => {
-        setNewComment(e.target.value)
-    }
+  const changeHandler = (e) => {
+    setNewComment(e.target.value);
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        setIsSuccessful(true)
-        api.postComments(article_id, newComment).then((addedComment) => {
-            setComments((currComments) => {
-                return [...currComments, addedComment]
-            })
-            setIsSuccessful(false)
-        }).catch((err) => {
-            setIsSuccessful(false)
-            setError("Something's gone worng, please refresh the page")
-        })
-        setNewComment("")
-    }
-    
-    if(isSuccessful) <p>Posting...</p>
-    if(error) return error;
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setIsSuccessful(true);
+    api
+      .postComments(article_id, newComment)
+      .then((addedComment) => {
+        setComments((currComments) => {
+          return [...currComments, addedComment];
+        });
+        setIsSuccessful(false);
+      })
+      .catch((err) => {
+        setIsSuccessful(false);
+        setError("Something's gone worng, please refresh the page");
+      });
+    setNewComment("");
+  };
+
+  if (isSuccessful) <p>Posting...</p>;
+  if (error) return error;
 
   return (
-    <form onSubmit={submitHandler} className='container form__container'>
-        <p></p>
-        <div id='comment-form'>
+    <form onSubmit={submitHandler} className="container form__container">
+      <div id="comment-form">
         {/* User avatar goes here */}
-        <input id='add-comment' value={newComment} onChange={changeHandler} type='text' placeholder='Add a comment...'></input>
-        <button id='post-comment-btn'>Post</button>
-        </div>
+        <textarea
+          id="add-comment"
+          value={newComment}
+          onChange={changeHandler}
+          type="text"
+          placeholder="Add a comment..."
+        />
+        <button id="post-comment-btn">Post</button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default CommentAdder
+export default CommentAdder;
