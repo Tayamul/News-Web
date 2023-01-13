5973992fa45f28c3188fa4ba340d1f2126a07914
Tayamul <rai_tayamul@hotmail.com>
2023-01-13 11:30:28 +0000

    Add drop-down tabs for sorting and ordering articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
 import * as api from "../utils/api";
-// import './articles.css'
 import { Link } from "react-router-dom";
 // import { Bars } from 'react-loading-icons'
 import IncrementArticleVote from "./IncrementArticleVote";
-
-
 import {
   Container,
   Stack,
@@ -26,13 +23,13 @@ import dayjs from "dayjs";
 const Articles = ({ articles, setArticles, topic }) => {
 
   const [sortBy, setSortBy] = useState('created_at')
-  const [order, setOrder] = useState('created_at')
+  const [order, setOrder] = useState('desc')
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
     api
-      .getArticles(topic)
+      .getArticles({topic, sortBy, order})
       .then(({ articles }) => {
         setArticles(articles);
         setIsLoading(false);
@@ -40,7 +37,7 @@ const Articles = ({ articles, setArticles, topic }) => {
       .catch((err) => {
         console.log(err, "ERR");
       });
-  }, [topic]);
+  }, [topic, sortBy, order]);
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -139,17 +136,3 @@ const Articles = ({ articles, setArticles, topic }) => {
 };
 
 export default Articles;
-
-//   <main className='articles__container'>
-//     <ul className='articles-box'>
-//       {articles.map((article) => {
-//         return <li key={article.article_id} className='articles-list'>
-//           <Link to={`/articles/${article.article_id}`}><img src='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png' alt='random image for now'/>
-//           <h3 className='articles-title'>{article.title}</h3>
-//           <h6 className='articles-author'>Author: {article.author}</h6>
-//           <h5 className='articles-topic'>Topic: {article.topic}</h5></Link>
-//           <IncrementArticleVote votes={article.votes} article_id={article.article_id}/>
-//         </li>
-//       })}
-//     </ul>
-// </main>
