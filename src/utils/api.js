a026eb56e750fb6c6f09a906ef75dc0903fe6581
Tayamul <rai_tayamul@hotmail.com>
2023-01-13 02:34:31 +0000

    Edit getArticles fetch functionality

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,20 +4,28 @@ const newsApi = axios.create({
   baseURL: "https://news-api-h06n.onrender.com/api",
 });
 
-export const getArticles = () => {
-  return newsApi.get(`/articles`).then((response) => {
-    console.log(response, "API")
-    return response.data;
-  });
-};
-
-export const getArticlesByQuery = (topic) => {
+export const getArticles = (topic) => {
   return newsApi.get(`/articles`, {
-    params: {topic: topic},
+    params: { topic: topic },
   }).then((response) => {
     return response.data;
   });
 };
+
+export const getTopics = () => {
+  return newsApi.get('/topics')
+  .then((response) => {
+    return response.data;
+  })
+}
+
+// export const getArticlesByQuery = (topic) => {
+//   return newsApi.get(`/articles`, {
+//     params: {topic: topic},
+//   }).then((response) => {
+//     return response.data;
+//   });
+// };
 
 export const getArticlesById = (article_id) => {
   return newsApi.get(`/articles/${article_id}`).then((response) => {
