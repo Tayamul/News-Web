07e758c2c4b14561d7ccd8e2ad43f29a7bbc7e2c
Tayamul <rai_tayamul@hotmail.com>
2023-01-17 22:09:09 +0000

    Add avatar in the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import CottageIcon from '@mui/icons-material/Cottage';
@@ -15,14 +15,16 @@ const Header = () => {
             <Typography variant="h4" color="#f6f6f6">News Web</Typography>
             
             <IconButton size='large' color="inherit" component={Link} to='/'><CottageIcon /></IconButton>
-            <Typography
+            {/* <Typography
               variant="text"
               color="inherit"
               component={Link}
               to={"/users"}
             >
               Logged in as: {user}
-            </Typography>
+            </Typography> */}
+            <Avatar src={user.avatar_url} alt='user image' component={Link}
+              to={"/users"}></Avatar>
         </Toolbar>
       </AppBar>
     </>
