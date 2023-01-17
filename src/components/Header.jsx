a6e2162a4b15b8031f23f9470637cccf61f32bf9
Tayamul <rai_tayamul@hotmail.com>
2023-01-17 18:12:37 +0000

    Add themes to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import { AppBar, Toolbar, Typography, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import CottageIcon from '@mui/icons-material/Cottage';
-import { IconButton } from '@mui/material';
+import { IconButton, useTheme } from '@mui/material';
 
 const Header = () => {
   const { user } = useContext(UserContext);
+  const {theme} = useTheme()
   return (
     <>
-      <AppBar color="secondary" position="relative">
+      <AppBar color='primary' position="relative">
         <Toolbar>
-        <Box sx={{display:"flex", alignItems:'center'}}>
-            <Typography variant="h4">News Web</Typography>
+            <Typography variant="h4" color="#f6f6f6">News Web</Typography>
             
             <IconButton size='large' component={Link} to='/'><CottageIcon /></IconButton>
             <Typography
@@ -23,7 +23,6 @@ const Header = () => {
             >
               Logged in as: {user}
             </Typography>
-          </Box>
         </Toolbar>
       </AppBar>
     </>
