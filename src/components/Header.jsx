cac8b534a8e270fbf41e61dda4a425f69ce8c635
Tayamul <rai_tayamul@hotmail.com>
2023-01-17 21:57:14 +0000

    Edit the header section of the webiste

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 import React, { useContext } from "react";
-import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import CottageIcon from '@mui/icons-material/Cottage';
-import { IconButton, useTheme } from '@mui/material';
+import { IconButton } from '@mui/material';
 
 const Header = () => {
   const { user } = useContext(UserContext);
-  const {theme} = useTheme()
+
   return (
     <>
-      <AppBar color='primary' position="relative">
-        <Toolbar>
+      <AppBar color='primary' position="relative" >
+        <Toolbar sx={{display:"flex", justifyContent:"space-between"}}>
             <Typography variant="h4" color="#f6f6f6">News Web</Typography>
             
-            <IconButton size='large' component={Link} to='/'><CottageIcon /></IconButton>
+            <IconButton size='large' color="inherit" component={Link} to='/'><CottageIcon /></IconButton>
             <Typography
               variant="text"
-              color="textSecondary"
+              color="inherit"
               component={Link}
               to={"/users"}
             >
